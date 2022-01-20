5627c147e7c4a01666bdc45c74e4da4657fd6e23
Luis Carrion <lcarrionalvarez@gmail.com>
2022-01-20 13:55:55 -0500

    Componentes lazy agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate } from 'react-router-dom';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import logo from '../logo.svg';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 export const Navigation = () => {
   return (
@@ -10,21 +11,21 @@ export const Navigation = () => {
           <img src={logo} alt="React logo" />
           <ul>
             <li>
-              <NavLink to="/home" className={({isActive}) => isActive? 'nav-active' : ''}>Home</NavLink> 
+              <NavLink to="/LazyPage1" className={({isActive}) => isActive? 'nav-active' : ''}>LazyPage1</NavLink> 
             </li>
             <li>
-              <NavLink to="/users" className={({isActive}) => isActive? 'nav-active' : ''}>Users</NavLink>
+              <NavLink to="/LazyPage2" className={({isActive}) => isActive? 'nav-active' : ''}>LazyPage2</NavLink>
             </li>
             <li>
-              <NavLink to="/About" className={({isActive}) => isActive? 'nav-active' : ''}>About</NavLink>
+              <NavLink to="/LazyPage3" className={({isActive}) => isActive? 'nav-active' : ''}>LazyPage3</NavLink>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/home" element={<h1>Home</h1>}/>
-          <Route path="/users" element={<h1>Users</h1>}/>
-          <Route path="/About" element={<h1>About</h1>}/>
-          <Route path="/*" element={<Navigate to="/home" replace/>}/>
+          <Route path="/LazyPage1" element={<LazyPage1 />}/>
+          <Route path="/LazyPage2" element={<LazyPage2 />}/>
+          <Route path="/LazyPage3" element={<LazyPage3 />}/>
+          <Route path="/*" element={<Navigate to="/LazyPage1" replace/>}/>
         </Routes>
       </div>
     </BrowserRouter>
