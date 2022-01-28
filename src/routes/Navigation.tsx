ecb37345b338f5a49ec81e65255ddcd2691fb49c
Luis Carrion <lcarrionalvarez@gmail.com>
2022-01-28 15:07:59 -0500

    ReactRouterD_V5_Simple

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,32 +1,42 @@
-import { BrowserRouter, Navigate } from 'react-router-dom';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from 'react-router-dom';
+
 import logo from '../logo.svg';
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div className='main-layout'>
         <nav>
           <img src={logo} alt="React logo" />
           <ul>
             <li>
-              <NavLink to="/home" className={({isActive}) => isActive? 'nav-active' : ''}>Home</NavLink> 
+              <NavLink to="/home" activeClassName='nav-active'>Home</NavLink> 
             </li>
             <li>
-              <NavLink to="/users" className={({isActive}) => isActive? 'nav-active' : ''}>Users</NavLink>
+              <NavLink to="/users" activeClassName='nav-active'>Users</NavLink>
             </li>
             <li>
-              <NavLink to="/About" className={({isActive}) => isActive? 'nav-active' : ''}>About</NavLink>
+              <NavLink to="/about" activeClassName='nav-active'>About</NavLink>
             </li>
           </ul>
         </nav>
-        <Routes>
-          <Route path="/home" element={<h1>Home</h1>}/>
-          <Route path="/users" element={<h1>Users</h1>}/>
-          <Route path="/About" element={<h1>About</h1>}/>
-          <Route path="/*" element={<Navigate to="/home" replace/>}/>
-        </Routes>
+        <Switch>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route path="/users">
+            <h1>Users</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
